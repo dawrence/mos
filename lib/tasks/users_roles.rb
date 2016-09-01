2036ab69c51048d2ae035a3ae28be12dc9c5ec5e
dawrence <dagudeloj@gmail.com>
2016-09-01 10:21:46 -0500

    users rake

---
+++ lib/tasks/users_roles.rb
@@ -0,0 +1,20 @@
+namespace :users do 
+    desc "Initialize by roles"
+    task :setup => [:environment] do 
+        User.all.each do |u|
+            u.roles=[]
+            case u.role
+            when 0
+                u.roles << Role.all
+            when 1
+                u.roles << Role.where(identifier: [:bills_edition,:view_kitchen,:closing_stage_management,:expenses_management,
+                                                    :inventories_management,:supplies_history,:bills_pending_report,:bills_today_paid_report,
+                                                    :bills_z_report,:bills_print,:bills_pending,:bills_prepared,:bills_payment,:bills_nullify,
+                                                    :bills_reports_history])
+            when 2
+                u.roles << Role.where(identifier: [:bills_edition,:bills_nullify])
+            end
+            u.save
+        end
+    end
+end
